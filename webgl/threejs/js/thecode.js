c072744c6f7c2edafaa790c9dbc8af16dfe58b23
Friðrik Njálsson <fridrikn@njalsson.is>
2017-11-20 19:50:22 +0000

    almost done with webgl

--- webgl/threejs/js/thecode.js
+++ webgl/threejs/js/thecode.js
@@ -1,39 +1,34 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
 
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//creating polyhedronGeometry
+//if i change the size of the browser, the program changes so.
+window.addEventListener( 'resize', function(){
+	var height = window.innerHeight;
+	var width  = window.innerWidth;
+	renderer.setSize( width, height);
+	camera.aspect = width / height;
+	camera.updateProjectionMatrix( );
 
-var verticesOfCube = [
-    -1,-1,-1,    1,-1,-1,    1, 1,-1,    -1, 1,-1,
-    -1,-1, 1,    1,-1, 1,    1, 1, 1,    -1, 1, 1,
-];
+} );
 
-var indicesOfFaces = [
-    2,1,0,    0,3,2,
-    0,4,7,    7,3,0,
-    0,1,5,    5,4,0,
-    1,2,6,    6,5,1,
-    2,3,7,    7,6,2,
-    4,5,6,    6,7,4
-];
-var geometry = new THREE.PolyhedronGeometry( verticesOfCube, indicesOfFaces, 6, 2 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//contolling
 
+controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.enableZoom = false;
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
 var cube = new THREE.Mesh( geometry, material );
-
 scene.add( cube );
 camera.position.z = 5;
-
 var animate = function () {
 	requestAnimationFrame( animate );
-	cube.rotation.x += 0.001;
+	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 	renderer.render(scene, camera);
 };
-
-
 animate();
