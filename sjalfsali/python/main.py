c1f2d9242d94c99ab26bfab3d2f14d647aa4a8d7
Friðrik Njálsson <fridrikn@njalsson.is>
2017-12-02 02:38:29 +0000

    moving to laptop

--- sjalfsali/python/main.py
+++ sjalfsali/python/main.py
@@ -1,20 +1,22 @@
-from lxml import html
+import json
 import requests
 
 
 
 #my ethereum wallet
-address = 'https://etherscan.io/address/0xe5c6daf760a1999ccc87afdb4c417d4f3097ccb0';
+address = 'http://api.etherscan.io/api?module=account&action=txlist&address=0xe5c6daf760a1999ccc87afdb4c417d4f3097ccb0&startblock=0&endblock=99999999&sort=asc&apikey=YGSWPV6U2U34VMFFS3C5EMSXXMPPZS2DR1B'
+
+r = requests.get(address)
+
+print(r.status_code)
 
 
-page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
-tree = html.fromstring(page.content)
-
-#This will create a list of buyers:
-buyers = tree.xpath('//div[@title="buyer-name"]/text()')
-#This will create a list of prices
-prices = tree.xpath('//span[@class="item-price"]/text()')
 
 
-print('Buyers: ', buyers)
-print('Prices: ', prices)
+json = r.json()
+print(json)
+
+
+
+for x in json['result'][0]:
+	print(x)
