e0993b3c139623c0b5abf11f88945c64dcb2e726
Friðrik Njálsson <fridrikn@njalsson.is>
2017-12-05 02:54:48 +0000

    changed motor functions

--- verk5/python/motortest.py
+++ verk5/python/motortest.py
@@ -8,18 +8,32 @@ pwm = GPIO.PWM(03, 50)
 pwm.start(90)
 pwm2.start(90)
 
-def setangle(angle):
-    duty = angle / 18 + 2
-    GPIO.output(05, True)
+
+
+
+def motor1(angle):
+	duty = angle / 18 + 2
     GPIO.output(03, True)
-    pwm2.ChangeDutyCycle(duty)
     pwm.ChangeDutyCycle(duty)
+    sleep(1)
     GPIO.output(03,False)
     pwm.ChangeDutyCycle(0)
+
+
+
+
+def motor2(angle):
+    duty = angle / 18 + 2
+	GPIO.output(05, True)
+    pwm2.ChangeDutyCycle(duty)
+    sleep(1)
     GPIO.output(05,False)
     pwm2.ChangeDutyCycle(0)
 
-setangle(180)
-sleep(0.5)
-setangle(90)
 
+
+motor1(180)
+motor1(90)
+
+motor2(180)
+motor2(90)
